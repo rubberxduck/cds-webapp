1f9148777e2e6cadeb9331649ea12a3d15a5a9c2
fscarpati@talend.com <fabrizio.scarpati@co-in.de>
2014-08-26 09:08:45 +0200

    Unit Test ergänzt.

---
+++ src/test/java/HelloWorldUnitTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class HelloWorldUnitTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
