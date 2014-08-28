84095759e98be2077aa54d55acccb6432647d61e
fscarpati@talend.com <fabrizio.scarpati@co-in.de>
2014-08-28 12:11:38 +0200

    Dynamische JSP ergänzt

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,5 +4,6 @@
 </head>
 <body>
 	<h2>Hello World!</h2>
+	<p>Aufgerufene URL: <%= request.getRequestURL() %></p>
 </body>
 </html>
--- src/test/java/HelloWorldIT.java
+++ src/test/java/HelloWorldIT.java
@@ -13,7 +13,7 @@ public class HelloWorldIT {
     @Test
     public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
         final WebClient webClient = new WebClient();
-        final HtmlPage page = webClient.getPage("http://localhost:8080/cds-webapp");
+        final HtmlPage page = webClient.getPage("http://localhost:8080/cds-webapp/");
         Assert.assertEquals("Hello World - Title", page.getTitleText());
 
         final String pageAsXml = page.asXml();
@@ -21,6 +21,7 @@ public class HelloWorldIT {
 
         final String pageAsText = page.asText();
         Assert.assertTrue(pageAsText.contains("Hello World!"));
+        Assert.assertTrue(pageAsText.contains("Aufgerufene URL: http://localhost:8080/cds-webapp/"));
 
         webClient.closeAllWindows();
     }
